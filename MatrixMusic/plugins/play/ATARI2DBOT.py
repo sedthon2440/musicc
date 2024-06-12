b31baea99a59a1d14dff5838e14d8c0ac3f43543
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-12 15:56:23 +0300

    Add files via upload

---
+++ MatrixMusic/plugins/play/ATARI2DBOT.py
@@ -0,0 +1,47 @@
+from pyrogram import filters
+from MatrixMusic import app
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtube_search import YoutubeSearch
+
+
+@app.on_message(filters.command(["بحث","يوت"], ""), group=4458890044)
+async def ytsearch(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        if len(message.command) < 2:
+            return await message.reply_text("جار البحث في سيرفرات سورس بلاك")
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text("يتم البحث الان ...")
+        results = YoutubeSearch(query, max_results=4).to_dict()
+        i = 0
+        text = ""
+        while i < 4:
+            text += f"≭︰العنـوان : ⦗ {results[i]['title']} ⦘\n"
+            text += f"≭︰المـدة : ⦗ {results[i]['duration']} ⦘\n"
+            text += f"≭︰المشاهـدات : ⦗ {results[i]['views']} ⦘\n"
+            text += f"≭︰القنـاة : ⦗ {results[i]['channel']} ⦘\n"
+            text += f"≭︰الرابـط : ⦗ https://youtube.com{results[i]['url_suffix']} ⦘\n\n"
+            i += 1
+        key = InlineKeyboardMarkup(
+            [
+                [
+                InlineKeyboardButton(
+                        "‹ 𝐁𝐥𝐚𝐜𝐤 𝐓𝐞𝐀𝐦 ›", url=f"https://t.me/KKC8C"),
+                ],[
+                    InlineKeyboardButton(
+                        text="‹ اغلاق ›",
+                        callback_data=f"forceclose abc|{message.from_user.id}",
+                    ),
+                ]
+            ]
+        )
+        await m.edit_text(
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        await message.reply_text(str(e))

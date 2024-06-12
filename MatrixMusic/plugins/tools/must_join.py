8e7a17a5a085b098ae29309ae1bca8bb7ac71bfa
LOLOW9W99 <asasynyy@gmail.com>
2024-06-12 16:44:00 +0200

    Update must_join.py

--- MatrixMusic/plugins/tools/must_join.py
+++ MatrixMusic/plugins/tools/must_join.py
@@ -5,27 +5,27 @@ from MatrixMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/KKC8C":  # Not compulsory
+    if not "https://t.me/F_U_01":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("KKC8C", msg.from_user.id)
+            await bot.get_chat_member("F_U_01", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/KKC8C".isalpha():
-                link = "https://t.me/KKC8C"
+            if "https://t.me/F_U_01".isalpha():
+                link = "https://t.me/F_U_01"
             else:
-                chat_info = await bot.get_chat("kkc8c")
+                chat_info = await bot.get_chat("F_U_01)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البوت .\n⌯︙قناة : t.me/KKC8C 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ حَبيبي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البوت .\n⌯︙قناة : t.me/F_U_01 🍓.\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("𝗕𝗟𝗔𝗖𝗞 |⌯𖤒˼ ˹🖤˼", url=link)]
+                        [InlineKeyboardButton("شعور", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @KKC8C !")
+        print(f"I'm not admin in the MUST_JOIN chat @F_U_01 !")

bbad47682aa155abdda11ba2d0abbc2111142c07
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-06-16 20:48:26 +0300

    Update play.py

--- MatrixMusic/plugins/play/play.py
+++ MatrixMusic/plugins/play/play.py
@@ -30,7 +30,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(   
-              text=f"𝗕𝗟𝗔𝗖𝗞 |⌯𖤒˼ ˹🖤˼", url=f"https://t.me/KKC8C",)                        
+              text=f"بغداد˼", url=f"https://t.me/lggbg",)                        
         ],        
     ]
 )
@@ -38,10 +38,10 @@ async def check_is_joined(message):
     try:
         userid = message.from_user.id
         user_name = message.from_user.first_name
-        status = await app.get_chat_member("KKC8C", userid)
+        status = await app.get_chat_member("lggbg", userid)
         return True
     except Exception:
-        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @KKC8C 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
+        await message.reply_text(f'┇عزيزي: {message.from_user.mention}\n┇أشتࢪك في قناة البوت أولاً.\n┇قناة البوت: @lggbg 🍓. ',reply_markup=force_btn,disable_web_page_preview=False)
         return False
 
 
